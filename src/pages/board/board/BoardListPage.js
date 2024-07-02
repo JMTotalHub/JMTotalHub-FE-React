b4d04aa8ece6fb8f6e087c1d4241742c4546e0b5
junmop95 <junmop950@gmail.com>
2024-07-02 14:59:48 +0900

    기본 게시판 기능 + CSS 구현 완료 2차

---
+++ src/pages/board/board/BoardListPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import BoardListComponent from '../../../components/board/board/BoardListComponent';
+
+const BoardListPage = () => {
+  return (
+    <div>
+      <h2>게시판 목록</h2>
+      <BoardListComponent />
+    </div>
+  );
+};
+
+export default BoardListPage;
