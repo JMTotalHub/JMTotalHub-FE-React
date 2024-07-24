035068678c6011d39dfe2c538d31dc74feaeb210
junmop95 <junmop950@gogle.com>
2024-07-25 00:20:02 +0900

    기초 계정 로직 완료

--- src/utils/api.js
+++ src/utils/api.js
@@ -8,32 +8,10 @@ const PROTOCOL = 'http';
 const API_URL = `${PROTOCOL}://${SERVER_IP}`;
 const BASE_URL = ENV === 'prod' ? `${API_URL}/api` : `${API_URL}`;
 
-// let api;
 const api = axios.create({
   baseURL: BASE_URL,
   withCredentials: true,
 });
-
-// if (ENV === 'dev') {
-//   console.log('dev 환경');
-//   // 개발 환경의 API 호출 헬퍼 함수
-//   api = axios.create({
-//     baseURL: API_URL,
-//     withCredentials: true,
-//   });
-// } else if (ENV === 'prod') {
-//   // 배포 환경의 API 호출 헬퍼 함수
-//   api = axios.create({
-//     baseURL: `${API_URL}/api`,
-//     withCredentials: true,
-//   });
-// } else {
-//   // 기본 API 호출 헬퍼 함수 (환경 변수 설정이 없을 경우)
-//   api = axios.create({
-//     baseURL: API_URL,
-//     withCredentials: true,
-//   });
-// }
 
 // 인터셉터 설정
 api.interceptors.request.use(
@@ -55,18 +33,23 @@ api.interceptors.response.use(
   async (error) => {
     console.log(error.response.data.err);
     const originalRequest = error.config;
+    console.log('원래 요청 : ', originalRequest);
 
     if (
       error.response.data.err === 'TokenExpiredError' &&
       !originalRequest._retry
     ) {
-      // 무한 루프 방지용
       originalRequest._retry = true;
 
       try {
-        const response = await axios.post(`${BASE_URL}/auth/new-token`);
+        await axios.post(
+          `${BASE_URL}/auth/new-token`,
+          {},
+          {
+            withCredentials: true,
+          }
+        );
 
-        // 기존 요청 재시도
         return api(originalRequest);
       } catch (refreshError) {
         console.log('토큰 갱신 실패', refreshError);
